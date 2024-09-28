682c3036a338d5753022db9755f3ebc7e5a90ac7
Facu Roly <frr.1991@gmail.com>
2024-09-28 12:59:38 +0200

    Definicion del modelo y repositorio

--- src/main/java/com/pruebatecnicabcnc/priceservice/repository/PricesRepository.java
+++ src/main/java/com/pruebatecnicabcnc/priceservice/repository/PricesRepository.java
@@ -1,4 +1,9 @@
 package com.pruebatecnicabcnc.priceservice.repository;
 
-public interface PricesRepository {
+import com.pruebatecnicabcnc.priceservice.model.Prices;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PricesRepository extends JpaRepository<Prices, Long> {
 }
