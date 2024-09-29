bfca50a071e5a000ef6c8d821a952598848bf6a1
Facu Roly <frr.1991@gmail.com>
2024-09-29 11:25:29 +0200

    Desarrollo del controlador, servicio con toda la logica de negocio.

--- src/main/java/com/pruebatecnicabcnc/priceservice/service/PricesService.java
+++ src/main/java/com/pruebatecnicabcnc/priceservice/service/PricesService.java
@@ -1,10 +1,8 @@
 package com.pruebatecnicabcnc.priceservice.service;
 
 import com.pruebatecnicabcnc.priceservice.dto.PricesDto;
-import com.pruebatecnicabcnc.priceservice.model.Prices;
-
 import java.time.LocalDateTime;
-import java.util.List;
+
 
 public interface PricesService {
 
